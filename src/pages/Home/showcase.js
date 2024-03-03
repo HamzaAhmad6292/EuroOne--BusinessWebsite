69246c90b005cfdd37e610bfab7adc4f6997f4dd
Hamza <hamzaahmad6292@gmail.com>
2024-03-04 00:26:04 +0500

    test

--- src/pages/Home/showcase.js
+++ src/pages/Home/showcase.js
@@ -41,7 +41,13 @@ function Showcase() {
         </div>
       </div>
 
-
+      <Fade direction="right" triggerOnce>
+        <img
+          src={img}
+          alt="Leafs"
+          className="lg:w-full h-48 object-cover h-screen sm:w-4/12 w-screen transition duration-300 ease-in-out hover:scale-110 "
+        />
+      </Fade>
     </div>
   );
 }
